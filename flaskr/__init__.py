68888eec8ded85a4051ed99ffcdfbe2b588134d1
kalyons11 <kevinandrewlyons@gmail.com>
2017-12-28 23:36:18 -0500

    Adding schema file.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,3 +4,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+# Simple route
+@app.route('/test')
+def test_page():
+    return 'Hello World'
---
+++ flaskr/test/test_app.py
@@ -0,0 +1,17 @@
+"""App test file.
+"""
+
+import unittest
+
+from flaskr import app
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_home(self):
+        res = self.app.get('/test')
+        data = res.data.decode()
+        self.assertEqual(data, 'Hello World')
